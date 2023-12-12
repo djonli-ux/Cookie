f253d94bfd234d0d52e301622dd3cf5d6065f67d
Jonatan Lifshits <djonli2003@gmail.com>
2023-12-12 12:56:03 +0200

    mod: index.php (adding container)

--- index.php
+++ index.php
@@ -10,24 +10,25 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-<h3 class="mb-3">Form</h3>
-<form>
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <div class="container">
+        <h3 class="mb-3">Form</h3>
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleColorInput" class="form-label">Choose theme color</label>
+                <input type="color" class="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-    <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="mb-3">
-        <label for="exampleColorInput" class="form-label">Choose theme color</label>
-        <input type="color" class="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
 <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
